c0231d25be2e57cd6b77fa540cd08ade45fdbd4d
NIghtINvader <muammarsadam13@gmail.com>
2022-10-03 22:35:47 +0700

    second

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -5,7 +5,18 @@ class Help extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Text("Asu"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Help"),
+      ),
+      body: Container(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Text("Login, menu 1 ada list anggota kami, menu 2 ada stopwatch, menu 3 ada list hobby kami, menu 4 ada logout, dibawah ada navbar bisa ke menu sama ke menu bantuan ya ini itu bantuannya, silahkan menikmati dada dan sampai jumpa ")
+          ],
+        ),
+      ),
+    );
   }
 }
